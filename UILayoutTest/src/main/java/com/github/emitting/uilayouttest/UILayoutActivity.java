6fc8248b18631ee26fbdde3b2bfba03c55131248
Steve Chang <stevzhg@gmail.com>
2015-07-11 18:50:03 +0800

    Page 113

---
+++ UILayoutTest/src/main/java/com/github/emitting/uilayouttest/UILayoutActivity.java
@@ -0,0 +1,13 @@
+package com.github.emitting.uilayouttest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class UILayoutActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout);
+    }
+}
