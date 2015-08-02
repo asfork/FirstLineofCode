741859561d4a9bfb4675a48c461563c9254e8d4e
Steve Chang <stevzhg@gmail.com>
2015-08-02 10:57:49 +0800

    Broadcast Test Page 204

---
+++ BroadcastTest/src/main/java/BootCompleteReceiver.java
@@ -0,0 +1,14 @@
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+/**
+ * Created by Steve on 2015/8/2.
+ */
+public class BootCompleteReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive (Context context, Intent intent) {
+        Toast.makeText(context, "Boot Complete", Toast.LENGTH_LONG).show();
+    }
+}
